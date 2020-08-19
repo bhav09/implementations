a804e9b0ad6aea24400fd2f91406553e61886f32
Bhavishya Pandit <50860603+bhav09@users.noreply.github.com>
2020-08-19 19:32:06 +0530

    Add files via upload

---
+++ automation/MS Paint/zigzag.py
@@ -0,0 +1,36 @@
+import pyautogui
+import time
+
+time.sleep(3)
+pyautogui.click()
+pyautogui.dragRel(100,0,duration=0.2)
+pyautogui.click()
+pyautogui.dragRel(20,75,duration=0.2)
+pyautogui.dragRel(25,-140,duration=0.2)
+pyautogui.dragRel(25,140,duration=0.2)
+pyautogui.dragRel(25,140,duration=0.2)
+#
+pyautogui.dragRel(25,-140,duration=0.2)
+pyautogui.dragRel(25,-175,duration=0.2)
+pyautogui.dragRel(25,240,duration=0.2)
+pyautogui.dragRel(25,-140,duration=0.2)
+pyautogui.dragRel(75,0,duration=0.2)
+pyautogui.dragRel(25,-130,duration=0.2)
+pyautogui.dragRel(25,300,duration=0.2)
+pyautogui.dragRel(25,-370,duration=0.2)
+pyautogui.dragRel(25,200,duration=0.2)
+pyautogui.dragRel(65,0,duration=0.2)
+pyautogui.dragRel(25,-140,duration=0.2)
+pyautogui.dragRel(25,200,duration=0.2)
+pyautogui.dragRel(25,100,duration=0.2)
+pyautogui.dragRel(25,-300,duration=0.2)
+
+
+
+
+
+
+
+
+
+
