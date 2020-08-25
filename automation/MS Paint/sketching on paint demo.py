eca8f7457872ae11719838c94f622cd6cf1da511
Bhavishya Pandit <50860603+bhav09@users.noreply.github.com>
2020-08-25 19:13:15 +0530

    Update sketching on paint demo.py

--- automation/MS Paint/sketching on paint demo.py
+++ automation/MS Paint/sketching on paint demo.py
@@ -1,36 +1,38 @@
-# GuideLine : 
+# GuideLine : Please do read the comments before running the code !
 #if you want to terminate the automation in midst of it, press alt+ctrl+del, then escape key.
 #it approximately takes 15-18 mins depending upon the edges it is detecting , so please be patient. 
 
-import pyautogui
-import time
-import cv2
-import numpy as np
+#dependencies
+
+import pyautogui #pip install pyautogui
+import time #inbuilt
+import cv2 #pip install opencv-python
+import numpy as np #pip install numpy
 
 pyautogui.FAILSAFE = True
-#time.sleep(3)
+#time.sleep(3) 3 sec delay
 
 video = cv2.VideoCapture(0)
 '''
-#video.set(3,500)
-#video.set(4,500)
+#video.set(3,500) #width
+#video.set(4,500) #height 
 '''
-video.set(10,100)
+video.set(10,100) #brightness 
 while True:
     _,img = video.read()
-    img_flip = cv2.flip(img,1) #flipping the image
+    img_flip = cv2.flip(img,1) #flipping the image , mirror image (exceptional)
     cv2.imshow('Video',img_flip)
     if cv2.waitKey(1) == 27: #press escape to capture the picture from the live video 
         image = img_flip
         break
-video.release()
-cv2.destroyAllWindows()
+video.release() #releasing the video capture 
+cv2.destroyAllWindows() #cleaning all the windows
 
 canny = cv2.Canny(image,130,180) #these are thresholds , one can vary these thresholds. In my case it worked for a range of (50,180) bcz I had a plane background.
-print(canny.shape)
+print(canny.shape) #printing the shape of the edged image.
 print(np.unique(canny))
 cv2.imshow('Canny',canny)
-cv2.waitKey(3000)
+cv2.waitKey(3000) #Images will be theere on your screen for a time of 3 seconds
 time.sleep(4) #now you have a time of 4 seconds to go to your paint window, it is advised to keep it open in the background so that that a lot of time isn't wasted.
 pyautogui.click()
 
@@ -43,4 +45,4 @@ for i in range(100,canny.shape[0]):   #you can change 100 to 0, I kept it 100 so
 
 cv2.imshow('Edges',canny)
 cv2.imshow('Captured Image',image)
-cv2.waitKey(0)
+cv2.waitKey(0) #infinite delay , can terminate this by pressing escape key
